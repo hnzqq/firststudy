e5b508a0a64f8ffe5c0d46b3a4db1e8b7408fed8
hnzqq <hnzhangqianqiang@163.com>
2018-09-29 10:04:14 +0800

    change

--- src/simplejava/com/zqq/StartMian.java
+++ src/simplejava/com/zqq/StartMian.java
@@ -1,11 +1,26 @@
 package simplejava.com.zqq;
 
+import java.util.ArrayList;
+
 public class StartMian {
 	/**
 	 * 整个学习运行程序的起点
 	 * @param args
 	 */
 public static void main(String[] args) {
-	
+	ArrayList<Integer> list = new ArrayList<Integer>();
+	list.add(1);
+	list.size();
+	list.toArray(new Integer[10]);
+	list.addAll(new ArrayList<Integer>());
+	list.contains(1);
+	list.get(0);
+	list.isEmpty();
+	list.iterator();
+	list.set(0, 12);
+	list.clear();
+	list.remove(0);
+	list.indexOf(1);
+	list.stream();
 }
 }
