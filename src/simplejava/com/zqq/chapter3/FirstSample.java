59e89bc7ab6c60abe57ae44fb3d68f70139bcfba
hnzqq <hnzhangqianqiang@163.com>
2018-09-29 12:22:30 +0800

    3.1 第一个简单Java程序

---
+++ src/simplejava/com/zqq/chapter3/FirstSample.java
@@ -0,0 +1,15 @@
+package simplejava.com.zqq.chapter3;
+
+/**
+ * 
+* @ClassName: FirstSample 
+* @Description: chapter3 
+* @author zhangqianqiang 
+* @date 2018年9月29日 上午10:38:28 
+*
+ */
+public class FirstSample {
+public static void main(String[] args) {
+	System.out.println("We will not use \"Hello World!\"");
+}
+}
